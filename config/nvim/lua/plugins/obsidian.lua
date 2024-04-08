ad47ee632bd03827c5c44328b8bf59a93a9e5d41
taemath <hirotaka081630@gmail.com>
2024-04-08 09:04:40 +0900

    feat(nvim) add obsidian.nvim

--- config/nvim/lua/plugins/obsidian.lua
+++ config/nvim/lua/plugins/obsidian.lua
@@ -25,5 +25,29 @@ return {
     ui = {
       enable = false,
     },
+
+    notes_subdir = "00_Inbox",
+    new_notes_location = "notes_subdir",
+
+    note_id_func = function(title)
+      local file_name
+      if title ~= nil then
+        file_name = title:gsub(" ", "-"):gsub("[^A-Za-z0-9-]", ""):lower()
+      else
+        file_name = tostring(os.time())
+      end
+      return file_name
+    end,
+
+    preferred_link_style = "wiki",
+
+    templates = {
+      subdir = "99_templates",
+      date_format = "%Y-%m-%d",
+      time_format = "%H:%M",
+    },
+  },
+  keys = {
+    { "<leader>zn", "<cmd>ObsidianNew<cr>", desc = "Obsidian new note" },
   },
 }
