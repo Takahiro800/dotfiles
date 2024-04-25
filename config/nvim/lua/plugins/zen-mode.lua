efa7480b623548c92a13ddb2fb2d2db2e59dfdc4
Takahiro Abe <hirotaka081630@gmail.com>
2024-04-25 16:38:33 +0900

    feat(nvim) add zen-mode

---
+++ config/nvim/lua/plugins/zen-mode.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "folke/zen-mode.nvim",
+    keys = {
+      { "<leader>zm", "<cmd>ZenMode<CR>", desc = "Zen mode" },
+    },
+    opts = {
+      window = {
+        width = 0.8,
+      },
+    },
+  },
+}
