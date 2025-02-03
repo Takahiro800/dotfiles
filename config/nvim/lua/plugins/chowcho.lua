6d194dfcea9f0ad599f58640a324c5228f63c1d7
Takahiro Abe <hirotaka081630@gmail.com>
2025-02-03 09:07:05 +0900

    feat(nvim) set chowcho.nvim

---
+++ config/nvim/lua/plugins/chowcho.lua
@@ -0,0 +1,9 @@
+return {
+  "tkmpypy/chowcho.nvim",
+  keys = {
+    { "<leader>ww", ":Chowcho<CR>", desc = "select window" },
+  },
+  opts = {
+    labels = { "a", "s", "d", "f", "w", "u", "h", "j", "k" },
+  },
+}
